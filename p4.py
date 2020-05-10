373c843a331d2d87c13346494082652adf7e93af
nyanhi <ksuzuki19937.mm@gmail.com>
2020-05-10 15:59:39 +0900

    solved upto problems 6

---
+++ p4.py
@@ -0,0 +1,19 @@
+# https://atcoder.jp/contests/pakencamp-2019-day3/tasks/pakencamp_2019_day3_c
+# brute-force, computational complexity O(
+
+N, M = map(int, input().split())
+A = []
+
+for i in range(N):
+    lst = list(map(int, input().split()))
+    A.append(lst)
+
+max_score = 0
+for i in range(M):
+    for j in range(i + 1, M):
+        score = 0
+        for k in range(N):
+            score += max(A[k][i], A[k][j])
+        max_score = max(max_score, score)
+
+print(max_score)
---
+++ p5.py
@@ -0,0 +1,17 @@
+# https://atcoder.jp/contests/abc095/tasks/arc096_a
+# brute-force, computational complexity O(max(X, Y))
+
+A, B, C, X, Y = map(int, input().split())
+
+
+INF = float('inf')
+min_cost = INF
+
+k = 0
+while X - k >= 0 or Y - k >= 0:
+    cost = A * max(0, X-k) + B * max(0, Y-k) + 2 * C * k
+    min_cost = min(min_cost, cost)
+    k += 1
+
+print(min_cost)
+
