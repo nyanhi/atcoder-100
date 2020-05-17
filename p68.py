af3b63a00564fe3f75b7e558b6fcc04a7b89edbe
nyanhi <ksuzuki19937.mm@gmail.com>
2020-05-17 18:41:11 +0900

    adding new answers

--- p68.py
+++ p68.py
@@ -1,7 +1,25 @@
 # coding=utf-8
-# http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=NTL_1_A&lang=jahttp://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=NTL_1_A&lang=ja
+# http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=NTL_1_A&lang=ja
+
 from math import sqrt
 
 n = int(input())
+orig_n = n
 
+factor = []
+
+i = 2
+while i <= sqrt(n):
+    if n % i == 0:
+        factor.append(i)
+        n //= i
+    else:
+        i += 1
+
+if n != 1:
+    factor.append(n)
+
+res = f'{orig_n}: ' + ' '.join(map(str, factor))
+
+print(res)
 
